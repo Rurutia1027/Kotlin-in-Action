cd5fe1aadc423ad2e1b4f63d21c2c335a0c37ec4
Rurutia1027 <rurutia1027@gmail.com>
2026-05-29 14:45:40 +0800

    impl order domain container

---
+++ order-service/order-container/src/main/kotlin/com/food/ordering/system/order/service/domain/OrderServiceApplication.kt
@@ -0,0 +1,25 @@
+package com.food.ordering.system.order.service.domain
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@EnableJpaRepositories(
+    basePackages = [
+        "com.food.ordering.system.order.service.dataaccess",
+        "com.food.ordering.system.dataaccess",
+    ],
+)
+@EntityScan(
+    basePackages = [
+        "com.food.ordering.system.order.service.dataaccess",
+        "com.food.ordering.system.dataaccess",
+    ],
+)
+@SpringBootApplication(scanBasePackages = ["com.food.ordering.system"])
+class OrderServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrderServiceApplication>(*args)
+}
